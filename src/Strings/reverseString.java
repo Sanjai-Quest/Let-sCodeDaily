b7240303e8cf5055065487ae0bf33a18e49dbfed
Sanjai-Quest <sanjaioff@gmail.com>
2026-08-26 18:32:47 +0530

    Completed Reverse a String Leetcode problem

---
+++ src/Strings/reverseString.java
@@ -0,0 +1,25 @@
+package Strings;
+
+import java.util.Arrays;
+
+public class reverseString {
+
+    public static char[] reverseString(char[] s) {
+        int left = 0, right = s.length - 1;
+        while(left <= right) {
+            char temp = s[left];
+            s[left] = s[right];
+            s[right] = temp;
+            left++;
+            right--;
+        }
+        return s;
+    }
+
+    public static void main(String[] args) {
+        char[] s = {'h','e','l','l','o'};
+        char[] res = reverseString(s);
+        System.out.println(Arrays.toString(res));
+    }
+
+}
