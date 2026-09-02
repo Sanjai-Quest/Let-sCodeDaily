2d65c2d26f04278ac2efd7823a3db40ce9e2bc90
Sanjai-Quest <sanjaioff@gmail.com>
2026-09-02 22:05:58 +0530

    Add reverse words solution

---
+++ src/Strings/ReverseWords.java
@@ -0,0 +1,25 @@
+package Strings;
+
+public class ReverseWords {
+
+    static class Solution {
+        public String reverseWords(String s) {
+            String[] words = s.split("\\s+");
+            StringBuilder res = new StringBuilder();
+
+            for (int i = words.length - 1; i >= 0; i--) {
+                res.append(words[i]);
+                if (i != 0) {
+                    res.append(" ");
+                }
+            }
+
+            return res.toString().trim();
+        }
+    }
+
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        System.out.println(solution.reverseWords("the sky is blue"));
+    }
+}
